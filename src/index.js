78df0922675df516c49b413ccee4a74ce85e79dd
xuqiang <xuqiang@wafersystems.com>
2017-09-15 18:02:59 +0800

    achieved color ring for saturation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './ColorRing';
+import App from './ColorRing-H';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App onChange={v => console.log(v)} radius={160} adjustAngle={9} changeBackground/>, document.getElementById('root'));
